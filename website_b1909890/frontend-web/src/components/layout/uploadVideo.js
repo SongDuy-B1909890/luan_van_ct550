5a738c4645dda6d4c76de6fe140014282d8b801e
B1909890 <duyb1909890@student.ctu.edu.vn>
2023-10-28 16:15:25 +0700

    update commit

--- website_b1909890/frontend-web/src/components/layout/uploadVideo.js
+++ website_b1909890/frontend-web/src/components/layout/uploadVideo.js
@@ -7,18 +7,17 @@ import * as Yup from "yup"
 const UploadVideoPage = () => {
 
     const validationSchema = Yup.object({
-        firstname: Yup.string().required('Vui lòng nhập tên'),
-        lastname: Yup.string().required('Vui lòng nhập họ'),
-        email: Yup.string().email('Địa chỉ email không hợp lệ').required('Vui lòng nhập email'),
-        password: Yup.string().min(6, 'Mật khẩu phải có ít nhất 6 ký tự').required('Vui lòng nhập mật khẩu'),
+        title: Yup.string().required('Vui lòng nhập tên'),
+        decription: Yup.string().required('Vui lòng nhập họ'),
+        category: Yup.string().email('Địa chỉ email không hợp lệ').required('Vui lòng nhập email'),
     });
 
     const formik = useFormik({
         initialValues: {
-            firstname: '',
-            lastname: '',
-            email: '',
-            password: '',
+            id_user: '',
+            title: '',
+            decription: '',
+            category: '',
         },
         validationSchema: validationSchema,
         onSubmit: (values) => {
@@ -48,7 +47,7 @@ const UploadVideoPage = () => {
 
                         <div className="sm:mx-auto sm:w-full sm:max-w-md mb-6">
                             <h2 className=" text-center text-3xl font-extrabold text-gray-900">
-                                Đăng Ký Tài Khoản
+                                Tải video
                             </h2>
                             <p className="mt-2 text-center text-sm text-gray-600">
                                 <Link
@@ -66,90 +65,86 @@ const UploadVideoPage = () => {
 
                                 <div>
                                     <label
-                                        htmlFor="firstname"
+                                        htmlFor="title"
                                         className="block text-sm font-medium text-gray-700"
                                     >
-                                        Họ
+                                        Tiêu đề
                                     </label>
                                     <div className="mt-1">
                                         <input
-                                            id="firstname"
-                                            name="firstname"
+                                            id="title"
+                                            name="title"
                                             type="text"
-                                            autoComplete="firstname"
-                                            placeholder="Nguyễn Văn"
-                                            value={formik.values.firstname} // Đặt giá trị của input từ state user
+                                            autoComplete="title"
+                                            placeholder="Title video"
+                                            value={formik.values.title} // Đặt giá trị của input từ state user
                                             onChange={formik.handleChange} // Gọi hàm formik.handleChange khi giá trị thay đổi
                                             className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                                         />
                                     </div>
-                                    {formik.errors.firstname && formik.touched.firstname && <div>{formik.errors.firstname}</div>}
+                                    {formik.errors.title && formik.touched.title && <div>{formik.errors.title}</div>}
                                 </div>
 
                                 <div>
                                     <label
-                                        htmlFor="lastname"
+                                        htmlFor="decription"
                                         className="block text-sm font-medium text-gray-700"
                                     >
-                                        Tên
+                                        Mô tả
                                     </label>
                                     <div className="mt-1">
                                         <input
-                                            id="lastname"
-                                            name="lastname"
-                                            type="text"
-                                            autoComplete="lastname"
-                                            placeholder="An"
-                                            value={formik.values.lastname}
+                                            id="decription"
+                                            name="decription"
+                                            type="address"
+                                            autoComplete="decription"
+                                            placeholder="Decription video"
+                                            value={formik.values.decription}
                                             onChange={formik.handleChange} // Gọi hàm formik.handleChange khi giá trị thay đổi
                                             className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                                         />
                                     </div>
-                                    {formik.errors.lastname && formik.touched.lastname && <div>{formik.errors.lastname}</div>}
+                                    {formik.errors.decription && formik.touched.decription && <div>{formik.errors.decription}</div>}
                                 </div>
 
                                 <div>
                                     <label
-                                        htmlFor="email"
+                                        htmlFor="category"
                                         className="block text-sm font-medium text-gray-700"
                                     >
-                                        Email
+                                        Danh mục
                                     </label>
                                     <div className="mt-1">
                                         <input
-                                            id="email"
-                                            name="email"
-                                            type="email"
-                                            autoComplete="email"
+                                            id="category"
+                                            name="category"
+                                            type="category"
+                                            autoComplete="category"
                                             placeholder="my@gmail.com"
-                                            value={formik.values.email}
+                                            value={formik.values.category}
                                             onChange={formik.handleChange} // Gọi hàm formik.handleChange khi giá trị thay đổi
                                             className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                                         />
 
                                     </div>
-                                    {formik.errors.email && formik.touched.email && <div>{formik.errors.email}</div>}
+                                    {formik.errors.category && formik.touched.category && <div>{formik.errors.category}</div>}
                                 </div>
 
                                 <div>
-                                    <label
-                                        htmlFor="password"
-                                        className="block text-sm font-medium text-gray-700"
-                                    >
-                                        Mật khẩu
+                                    <label htmlFor="video" className="block text-sm font-medium text-gray-700">
+                                        Video
                                     </label>
                                     <div className="mt-1">
                                         <input
-                                            id="password"
-                                            name="password"
-                                            type="password"
-                                            autoComplete="password"
-                                            value={formik.values.password}
+                                            id="video"
+                                            name="video"
+                                            type="file" // Thay đổi type thành "file" để cho phép tải tệp
+                                            accept="video/*" // Chỉ chấp nhận các tệp video
                                             onChange={formik.handleChange} // Gọi hàm formik.handleChange khi giá trị thay đổi
                                             className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                                         />
                                     </div>
-                                    {formik.errors.password && formik.touched.password && <div>{formik.errors.password}</div>}
+                                    {formik.errors.video && formik.touched.video && <div>{formik.errors.video}</div>}
                                 </div>
 
                                 <div className="z-10">
