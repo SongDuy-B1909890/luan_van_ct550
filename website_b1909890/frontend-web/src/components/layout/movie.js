240531ef3f8bb4c1d541aaf853d88e27d16c0601
B1909890 <duyb1909890@student.ctu.edu.vn>
2023-10-29 18:42:56 +0700

    update commit

--- website_b1909890/frontend-web/src/components/layout/movie.js
+++ website_b1909890/frontend-web/src/components/layout/movie.js
@@ -4,40 +4,45 @@ import axios from 'axios';
 
 const MoviePage = () => {
     const [videos, setVideos] = useState([]);
+
     useEffect(() => {
-        // Gọi API để lấy danh sách video
         axios
             .get('http://localhost:5000/api/videos')
             .then((response) => {
-                // Xử lý thành công
                 const videosData = response.data;
                 setVideos(videosData);
             })
             .catch((error) => {
-                // Xử lý lỗi, ví dụ: hiển thị thông báo lỗi
                 console.error(error);
             });
     }, []);
 
     return (
-        <div className="w-full h-full overflow-auto bg-white">
-            <div className="flex flex-wrap justify-center items-center mt-[105px]">
-                {videos.map((video, index) => (
-                    <div key={index} className="flex justify-center items-center">
-                        <div className="mb-[45px] overflow-hidden" style={{ borderRadius: '10px' }}>
-                            <ReactPlayer
-                                url={video.url_video}
-                                width="960px"
-                                height="540px"
-                                controls
-                                allowFullScreen={true}
-                                loading="lazy"
-                                preload="true"
-                            />
+        <div className="w-full h-full overflow-auto bg-gray-100">
+            {videos.map((video, index) => (
+                <div key={index} className="flex flex-wrap justify-center items-center h-screen">
+
+                    <div className="min-w-[1000px] min-h-[625px] max-w-[1000px] max-h-[625px] bg-white rouder-xl flex justify-center rounded-xl">
+                        <div className="overflow-hidden " >   {/* */}
+                            <div className="mt-5 ">
+                                <ReactPlayer
+                                    url={video.url_video}
+                                    width="960px"
+                                    height="540px"
+                                    controls
+                                    allowFullScreen={true}
+                                    loading="lazy"
+                                    preload="true"
+                                />
+                            </div>
+                            <div className="mt-2 mb-2">
+                                <span>{video.title}</span>
+                            </div>
+
                         </div>
                     </div>
-                ))}
-            </div>
+                </div>
+            ))}
         </div>
     );
 };
