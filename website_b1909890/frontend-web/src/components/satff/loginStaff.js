d13a7448835e79bfb887ddb05b25a267c7acc2d1
B1909890 <duyb1909890@student.ctu.edu.vn>
2023-11-03 15:34:34 +0700

    update commit

--- website_b1909890/frontend-web/src/components/satff/loginStaff.js
+++ website_b1909890/frontend-web/src/components/satff/loginStaff.js
@@ -100,43 +100,7 @@ const LoginAdminPage = ({ closeModal }) => {
                                 </div>
                             </div>
 
-                            {/* <div className="flex items-center">
-                                <input
-                                    id="remember_me"
-                                    name="remember_me"
-                                    type="checkbox"
-                                    className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-                                />
-                                <label
-                                    htmlFor="remember_me"
-                                    className="ml-2 block text-sm text-gray-900"
-                                >
-                                    Ghi nhớ đăng nhập
-                                </label>
-                            </div> */}
                         </div>
-
-                        {/* <div className="flex items-center justify-between ">
-                            <div className="text-sm">
-                                <Link
-                                    to="/Register"
-                                    className="font-medium text-indigo-600 hover:text-indigo-500"
-
-                                >
-                                    <b>Đăng ký tài khoản</b>
-                                </Link>
-                            </div>
-
-                            <div className="text-sm">
-                                <Link
-                                    to="/forgotPassword"
-                                    className="font-medium text-indigo-600 hover:text-indigo-500"
-
-                                >
-                                    <b>Quên mật khẩu?</b>
-                                </Link>
-                            </div>
-                        </div> */}
 
                         <div className="py-6 flex justify-center  space-x-36 ">
                             <button
