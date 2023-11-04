7e1ee83a045b47fe2a4eae22699e28f02bbb0857
B1909890 <duyb1909890@student.ctu.edu.vn>
2023-11-04 09:32:20 +0700

    update commit

--- website_b1909890/frontend-web/src/components/auth/login.js
+++ website_b1909890/frontend-web/src/components/auth/login.js
@@ -57,9 +57,9 @@ const LoginPage = ({ closeModal }) => {
 
         <div className="sm:mx-auto sm:w-full sm:max-w-md ">
 
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          <h1 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
             Đăng Nhập Tài Khoản
-          </h2>
+          </h1>
 
         </div>
 
