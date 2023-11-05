7a9ee7eea39606f3cd8e5a25d6b55923fbfe8fd1
B1909890 <duyb1909890@student.ctu.edu.vn>
2023-11-05 10:51:11 +0700

    update commit

--- website_b1909890/frontend-web/src/components/satff/staffGroup01.js
+++ website_b1909890/frontend-web/src/components/satff/staffGroup01.js
@@ -12,46 +12,132 @@ const closeLogout = () => {
 
 const StaffGroup01Page = () => {
 
-    return (
-        <div>
-
+    if (staff.level === 1) { // giao diện trang nhân viên sơ tuyển nhóm 01
+        return (
             <div>
-                <div className="text-center mt-8 ml-8">
-                    Trang Nhân Viên Nhóm 1
+
+                <div>
+                    <div className="text-center mt-8 ml-8">
+                        Trang Nhân Viên Sơ Tuyển Nhóm 01
+                    </div>
+                    <div className="float-right mr-4">
+                        <button
+                            className="flex justify-center items-center text-blue-500 mt-1 w-[120px] h-10 hover:bg-blue-100 rounded-full border border-blue-500"
+                            title="Đăng xuất"
+                            onClick={closeLogout}
+                        >
+                            <div className="mr-2">
+                                <AccountCircleIcon />
+                            </div>
+                            Sign out
+                        </button>
+                        {staff.id}
+                    </div>
                 </div>
-                <div className="float-right mr-4">
-                    <button
-                        className="flex justify-center items-center text-blue-500 mt-1 w-[120px] h-10 hover:bg-blue-100 rounded-full border border-blue-500"
-                        title="Đăng xuất"
-                        onClick={closeLogout}
-                    >
-                        <div className="mr-2">
-                            <AccountCircleIcon />
-                        </div>
-                        Sign out
-                    </button>
-                    {staff.id}
+
+                <div>
+                    <ul>
+                        <li className='mb-2 hover:bg-gray-300 w-[160px] ml-8 tex-center'>
+                            Quản lý danh mục
+                        </li>
+                        <li className='mb-2 hover:bg-gray-300 w-[160px] ml-8 tex-center'>
+                            Quản lý video
+                        </li>
+                        <li className='mb-2 hover:bg-gray-300 w-[160px] ml-8 tex-center'>
+                            Quản lý bình luận
+                        </li>
+                        <li className='mb-2 hover:bg-gray-300 w-[160px] ml-8 tex-center'>
+                            Quản lý báo cáo
+                        </li>
+                    </ul>
                 </div>
             </div>
+        );
 
+    } if (staff.level === 2) { // Giao diện trang tổ phản biện nhóm 01
+        return (
             <div>
-                <ul>
-                    <li className='mb-2 hover:bg-gray-300 w-[160px] ml-8 tex-center'>
-                        Quản lý danh mục
-                    </li>
-                    <li className='mb-2 hover:bg-gray-300 w-[160px] ml-8 tex-center'>
-                        Quản lý video
-                    </li>
-                    <li className='mb-2 hover:bg-gray-300 w-[160px] ml-8 tex-center'>
-                        Quản lý bình luận
-                    </li>
-                    <li className='mb-2 hover:bg-gray-300 w-[160px] ml-8 tex-center'>
-                        Quản lý báo cáo
-                    </li>
-                </ul>
+
+                <div>
+                    <div className="text-center mt-8 ml-8">
+                        Trang Tổ Phản Biện Nhóm 01
+                    </div>
+                    <div className="float-right mr-4">
+                        <button
+                            className="flex justify-center items-center text-blue-500 mt-1 w-[120px] h-10 hover:bg-blue-100 rounded-full border border-blue-500"
+                            title="Đăng xuất"
+                            onClick={closeLogout}
+                        >
+                            <div className="mr-2">
+                                <AccountCircleIcon />
+                            </div>
+                            Sign out
+                        </button>
+                        {staff.id}
+                    </div>
+                </div>
+
+                <div>
+                    <ul>
+                        <li className='mb-2 hover:bg-gray-300 w-[160px] ml-8 tex-center'>
+                            Quản lý danh mục
+                        </li>
+                        <li className='mb-2 hover:bg-gray-300 w-[160px] ml-8 tex-center'>
+                            Quản lý video
+                        </li>
+                        <li className='mb-2 hover:bg-gray-300 w-[160px] ml-8 tex-center'>
+                            Quản lý bình luận
+                        </li>
+                        <li className='mb-2 hover:bg-gray-300 w-[160px] ml-8 tex-center'>
+                            Quản lý báo cáo
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
-    );
+        );
+
+    } else { // Giao diện trang quản lý trưởng nhóm 01
+        return (
+            <div>
+
+                <div>
+                    <div className="text-center mt-8 ml-8">
+                        Trang Quản Lý Trưởng Nhóm 01
+                    </div>
+                    <div className="float-right mr-4">
+                        <button
+                            className="flex justify-center items-center text-blue-500 mt-1 w-[120px] h-10 hover:bg-blue-100 rounded-full border border-blue-500"
+                            title="Đăng xuất"
+                            onClick={closeLogout}
+                        >
+                            <div className="mr-2">
+                                <AccountCircleIcon />
+                            </div>
+                            Sign out
+                        </button>
+                        {staff.id}
+                    </div>
+                </div>
+
+                <div>
+                    <ul>
+                        <li className='mb-2 hover:bg-gray-300 w-[160px] ml-8 tex-center'>
+                            Quản lý danh mục
+                        </li>
+                        <li className='mb-2 hover:bg-gray-300 w-[160px] ml-8 tex-center'>
+                            Quản lý video
+                        </li>
+                        <li className='mb-2 hover:bg-gray-300 w-[160px] ml-8 tex-center'>
+                            Quản lý bình luận
+                        </li>
+                        <li className='mb-2 hover:bg-gray-300 w-[160px] ml-8 tex-center'>
+                            Quản lý báo cáo
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        );
+    }
 };
 
 export default StaffGroup01Page;
--- website_b1909890/frontend-web/src/components/satff/staffGroup03.js
+++ website_b1909890/frontend-web/src/components/satff/staffGroup03.js
@@ -12,45 +12,132 @@ const closeLogout = () => {
 
 const StaffGroup03Page = () => {
 
-    return (
-        <div  > {/* backdrop-blur-sm */}
+    if (staff.level === 1) { // giao diện trang nhân viên sơ tuyển nhóm 03
+        return (
             <div>
-                <div className="text-center mt-8 ml-8">
-                    Trang Nhân Viên Nhóm 3
-                </div>
-                <div className="float-right mr-4">
-                    <button
-                        className="flex justify-center items-center text-blue-500 mt-1 w-[120px] h-10 hover:bg-blue-100 rounded-full border border-blue-500"
-                        title="Đăng xuất"
-                        onClick={closeLogout}
-                    >
-                        <div className="mr-2">
-                            <AccountCircleIcon />
-                        </div>
-                        Sign out
-                    </button>
-                    {staff.id}
-                </div>
-            </div>
 
-            <div>
-                <ul>
-                    <li className='mb-2 hover:bg-gray-300 w-[160px] ml-8 tex-center'>
-                        Quản lý danh mục
-                    </li>
-                    <li className='mb-2 hover:bg-gray-300 w-[160px] ml-8 tex-center'>
-                        Quản lý video
-                    </li>
-                    <li className='mb-2 hover:bg-gray-300 w-[160px] ml-8 tex-center'>
-                        Quản lý bình luận
-                    </li>
-                    <li className='mb-2 hover:bg-gray-300 w-[160px] ml-8 tex-center'>
-                        Quản lý báo cáo
-                    </li>
-                </ul>
+                <div>
+                    <div className="text-center mt-8 ml-8">
+                        Trang Nhân Viên Sơ Tuyển Nhóm 03
+                    </div>
+                    <div className="float-right mr-4">
+                        <button
+                            className="flex justify-center items-center text-blue-500 mt-1 w-[120px] h-10 hover:bg-blue-100 rounded-full border border-blue-500"
+                            title="Đăng xuất"
+                            onClick={closeLogout}
+                        >
+                            <div className="mr-2">
+                                <AccountCircleIcon />
+                            </div>
+                            Sign out
+                        </button>
+                        {staff.id}
+                    </div>
+                </div>
+
+                <div>
+                    <ul>
+                        <li className='mb-2 hover:bg-gray-300 w-[160px] ml-8 tex-center'>
+                            Quản lý danh mục
+                        </li>
+                        <li className='mb-2 hover:bg-gray-300 w-[160px] ml-8 tex-center'>
+                            Quản lý video
+                        </li>
+                        <li className='mb-2 hover:bg-gray-300 w-[160px] ml-8 tex-center'>
+                            Quản lý bình luận
+                        </li>
+                        <li className='mb-2 hover:bg-gray-300 w-[160px] ml-8 tex-center'>
+                            Quản lý báo cáo
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
-    );
+        );
+
+    } if (staff.level === 2) { // Giao diện trang tổ phản biện nhóm 03
+        return (
+            <div>
+
+                <div>
+                    <div className="text-center mt-8 ml-8">
+                        Trang Tổ Phản Biện Nhóm 03
+                    </div>
+                    <div className="float-right mr-4">
+                        <button
+                            className="flex justify-center items-center text-blue-500 mt-1 w-[120px] h-10 hover:bg-blue-100 rounded-full border border-blue-500"
+                            title="Đăng xuất"
+                            onClick={closeLogout}
+                        >
+                            <div className="mr-2">
+                                <AccountCircleIcon />
+                            </div>
+                            Sign out
+                        </button>
+                        {staff.id}
+                    </div>
+                </div>
+
+                <div>
+                    <ul>
+                        <li className='mb-2 hover:bg-gray-300 w-[160px] ml-8 tex-center'>
+                            Quản lý danh mục
+                        </li>
+                        <li className='mb-2 hover:bg-gray-300 w-[160px] ml-8 tex-center'>
+                            Quản lý video
+                        </li>
+                        <li className='mb-2 hover:bg-gray-300 w-[160px] ml-8 tex-center'>
+                            Quản lý bình luận
+                        </li>
+                        <li className='mb-2 hover:bg-gray-300 w-[160px] ml-8 tex-center'>
+                            Quản lý báo cáo
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        );
+
+    } else { // Giao diện trang quản lý trưởng nhóm 03
+        return (
+            <div>
+
+                <div>
+                    <div className="text-center mt-8 ml-8">
+                        Trang Quản Lý Trưởng Nhóm 03
+                    </div>
+                    <div className="float-right mr-4">
+                        <button
+                            className="flex justify-center items-center text-blue-500 mt-1 w-[120px] h-10 hover:bg-blue-100 rounded-full border border-blue-500"
+                            title="Đăng xuất"
+                            onClick={closeLogout}
+                        >
+                            <div className="mr-2">
+                                <AccountCircleIcon />
+                            </div>
+                            Sign out
+                        </button>
+                        {staff.id}
+                    </div>
+                </div>
+
+                <div>
+                    <ul>
+                        <li className='mb-2 hover:bg-gray-300 w-[160px] ml-8 tex-center'>
+                            Quản lý danh mục
+                        </li>
+                        <li className='mb-2 hover:bg-gray-300 w-[160px] ml-8 tex-center'>
+                            Quản lý video
+                        </li>
+                        <li className='mb-2 hover:bg-gray-300 w-[160px] ml-8 tex-center'>
+                            Quản lý bình luận
+                        </li>
+                        <li className='mb-2 hover:bg-gray-300 w-[160px] ml-8 tex-center'>
+                            Quản lý báo cáo
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        );
+    }
 };
 
 export default StaffGroup03Page;
