dfdec075fba6a1c25818a68bb939a12545875163
B1909890 <duyb1909890@student.ctu.edu.vn>
2023-11-05 11:55:35 +0700

    update commit

--- website_b1909890/frontend-web/src/components/satff/staffGroup01.js
+++ website_b1909890/frontend-web/src/components/satff/staffGroup01.js
@@ -15,26 +15,27 @@ const StaffGroup01Page = () => {
     if (staff.level === 1) { // giao diện trang nhân viên sơ tuyển nhóm 01
         return (
             <div>
-
+                {/* HeaderPage  */}
                 <div>
                     <div className="text-center mt-8 ml-8">
                         Trang Nhân Viên Sơ Tuyển Nhóm 01
-                    </div>
-                    <div className="float-right mr-4">
-                        <button
-                            className="flex justify-center items-center text-blue-500 mt-1 w-[120px] h-10 hover:bg-blue-100 rounded-full border border-blue-500"
-                            title="Đăng xuất"
-                            onClick={closeLogout}
-                        >
-                            <div className="mr-2">
-                                <AccountCircleIcon />
-                            </div>
-                            Sign out
-                        </button>
-                        {staff.id}
-                    </div>
-                </div>
 
+                        <div className=" flex justify-center items-center float-right mr-4">
+                            <span className="mr-4">{staff.name}</span>
+                            <button
+                                className="flex justify-center items-center text-blue-500 mt-1 w-[120px] h-10 hover:bg-blue-100 rounded-full border border-blue-500"
+                                title="Đăng xuất"
+                                onClick={closeLogout}
+                            >
+                                <div className="mr-2">
+                                    <AccountCircleIcon />
+                                </div>
+                                Sign out
+                            </button>
+
+                        </div></div>
+                </div>
+                {/* Nội dung */}
                 <div>
                     <ul>
                         <li className='mb-2 hover:bg-gray-300 w-[160px] ml-8 tex-center'>
