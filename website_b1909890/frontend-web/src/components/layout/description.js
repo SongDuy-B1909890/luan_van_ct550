d08d7771f2be9a0635efdd27b81fed9b21443698
B1909890 <duyb1909890@student.ctu.edu.vn>
2023-12-06 17:50:20 +0700

    update commit

--- website_b1909890/frontend-web/src/components/layout/description.js
+++ website_b1909890/frontend-web/src/components/layout/description.js
@@ -7,8 +7,8 @@ const DescriptionPage = (props) => {
         <div className="flex w-[450px] h-[675px] py-5 bg-white rounded-xl border shadow overflow-auto">
             <div className="w-full h-full custom-scrollbar-content custom-scrollbar ml-5 mr-5 ">
                 <div className="text-center flex">
-                    <h1 className="text-lg font-bold">Mô tả </h1>
-                    <span className="text-lg text-right ml-auto text-blue-900"> Id: {props.values.cloudinaryId}</span>
+                    <h1 className="text-xl font-bold bg-yellow-600 text-white px-5 py-1 rounded-full">Mô tả </h1>
+                    <span className="font-bold rounded-full bg-gray-200 px-3 py-1 text-right ml-auto text-blue-900"> ID: {props.values.cloudinaryId}</span>
                 </div>
                 <div className="ml-2 mb-2 mt-2">
                     {props.values.description}
